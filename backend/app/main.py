bfbdd377c2395aa080714911d152a4e4faff22bb
sergiorodriguez-sudo <sergiorodriguezredondo@gmail.com>
2026-01-29 14:22:17 -0300

    Fix: Force uvicorn command in Dockerfile with dynamic PORT

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,4 @@ app = FastAPI()
 def root():
     return {"message": "Hello form Cloud Run! Deployment Successful."}
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+
